2859de4936062323274f3b2245219008aeec51fc
GabrielGalati <gabrielgalati.js@gmail.com>
2020-08-29 13:07:08 -0400

    h

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
+/* control de la flecha para subir el scroll */
+
 $(document).ready(function () {
     $('.ir-arriba').click(function () {
         $('body,html').animate({
             scrollTop: '0px'
         }, 500);
     });
-
+    /*mostrar flecha cuando se baje crool */
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
             $('.ir-arriba').slideDown(300)
@@ -12,17 +14,19 @@ $(document).ready(function () {
             $('.ir-arriba').slideUp(300)
         }
     })
-
+    // movimiento acerca de con los botones
     $("#button-acerca-de").click(function () {
         $('html, body').animate({
             scrollTop: $("#section-about").offset().top
         }, 500);
     });
+    // movminemto de los proyectos con boton
     $("#button-proyectos").click(function () {
         $('html, body').animate({
             scrollTop: $("#section-contacto").offset().top
         }, 500);
     });
+    // movimiento de contacto con el scroll
     $("#button-contacto").click(function () {
         $('html, body').animate({
             scrollTop: $("#myDiv").offset().top
